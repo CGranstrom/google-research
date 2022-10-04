13b7cd0fb2ce749ccec0619b029eb3703fd7a1a8
Chris Granstrom <45301498+CGranstrom@users.noreply.github.com>
2022-10-04 10:45:47 -0700

    fix tensorflow error

--- smurf/smurf_net.py
+++ smurf/smurf_net.py
@@ -29,6 +29,8 @@ import smurf_utils
 from smurf_models import pwc_model
 from smurf_models import raft_model
 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 @gin.configurable
 class SMURFNet(object):
